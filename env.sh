dbf7b21d0a7a59ceceb02564fe8580f76be29d16
Xiaowen Zhang <seanxwzhang@gmail.com>
2019-09-15 22:32:26 -0900

    fix

--- env.sh
+++ env.sh
@@ -5,9 +5,9 @@ export PGHOST="localhost"
 export PGDATABASE="wrds"
 export PGUSER="wrds"
 
-PG_ACCESS_LINE=`wrds-pgdata.wharton.upenn.edu:9737:wrds:$WRDS_ID:$WRDS_PASS`
-PG_ACCESS_PATH=`~/.pgpass`
+PG_ACCESS_LINE="wrds-pgdata.wharton.upenn.edu:9737:wrds:$WRDS_ID:$WRDS_PASS"
+PG_ACCESS_PATH="$HOME/.pgpass"
 touch $PG_ACCESS_PATH
 echo "Setting up pgacess file"
-grep -qxF $PG_ACCESS_LINE $PG_ACCESS_PATH || echo $PG_ACCESS_LINE >> $PG_ACCESS_PATH
+grep -qxF "$PG_ACCESS_LINE" $PG_ACCESS_PATH || echo "$PG_ACCESS_LINE" >> $PG_ACCESS_PATH
 
